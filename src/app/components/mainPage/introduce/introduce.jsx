ba3f882959b555122a750e53c7b6d9b4487e08c0
lielsmoon <baluzaveta@gmail.com>
2022-02-05 01:09:05 +0200

    impoved page about me

--- src/app/components/mainPage/introduce/introduce.jsx
+++ src/app/components/mainPage/introduce/introduce.jsx
@@ -4,9 +4,10 @@ import "./introduce.css";
 const Introduce = () => {
     return (
         <div className="introduce">
-            <h5 className="introduce_text">
-                Добро пожаловать в мой блог полезного питания
-            </h5>
+            <div className="introduce_text">
+                Добро пожаловать в мой блог{" "}
+                <div>Здесь вы найдете много информации о пользе продуктов</div>
+            </div>
         </div>
     );
 };
